11707bec057191359341eac9d64af156c5f877f9
alon0608 <alonsh0608@gmail.com>
2024-12-16 22:03:19 +0200

    changed all the minus checker

--- Minus_Check.py
+++ Minus_Check.py
@@ -1,66 +1,50 @@
-from os import remove
-import Operators_dict
-from Operators_dict import Operators
-from os import remove
-import Operators_dict
-from Operators_dict import Operators
+from itertools import filterfalse
+from symbol import continue_stmt
 
+from lib2to3.fixer_util import String
+from os import remove
+from Custom_Exception_Class import TwoOrMoreTildas, MissingOperandError,NegativeFactorial,NegativeFactorialSum,NegativeSqrt,TooMuchOperatorsInARow
+import Operators_dict
+from Operators_dict import Operators
 
 def minus_checker(lst):
     count=0
-    number=1
     index=0
     tilda_check=0
     while index<len(lst) :
+        is_unary_before = False
         if is_unary(lst,index,tilda_check) :
-            lst[index]="<"
-            count+=1
-        elif index==len(lst)-1 and lst[index]=="~":
-            raise MissingOperandError("~")
-        elif lst[index]=="~" and (index!=0 and lst[index-1] in ("<","-") and count>0):
-            raise TooMuchOperatorsInARow("~","-")
+            while index<len(lst) and lst[index]=="-":
+                lst[index]="<"
+                is_unary_before=True
+                index+=1
         elif lst[index]=="~":
-            if tilda_check==1:
-                raise TwoOrMoreTildas()
-            tilda_check=1
-            index+=1
             count+=1
+            tilda_exception(lst,index,count,tilda_check)
+            tilda_check=1
         elif lst[index] in Operators and count==0 and index!=0 and lst[index-1]!="(" and lst[index] not in ("!","#") :
             count+=1
-            index+=1
         elif lst[index]=="-":
-            number*=-1
+            lst[index]="_"
             count+=1
-            lst.pop(index)
-        elif isinstance(lst[index],int) or isinstance(lst[index],float) or lst[index].isdigit():
-            lst[index] = int(lst[index])
-            count=0
-            lst[index]*=number
-            tilda_check = 0
-            number=1
-            lst[index]=str(lst[index])
-            index+=1
-        elif lst[index]=="(" and is_minus_before_parenthesis(number,tilda_check):
-            if lst[index-1]=="~":
-                lst[index-1]="_"
-            else:
-                lst.insert(index,"_")
-            index+=2
-            number=1
-            tilda_check=0
-        elif lst[index]=="(" and not is_minus_before_parenthesis(number,tilda_check):
-            if lst[index-1]=="~":
-                lst.pop(index-1)
-                index-=1
-            number=1
-            tilda_check=0
-            index+=1
         else:
-            index+=1
+            count=0
+        if is_unary_before==True:
+            index-=1
+        index+=1
     return lst
+
 def is_unary(lst,index,tilda_check):
     if (index == 0 and lst[index] == "-" and tilda_check == 0) or (
             index != 0 and lst[index - 1] == "(" and lst[index] == "-" and tilda_check == 0):
         return True
-def is_minus_before_parenthesis(number,tilda_check):
-    return (number==1and tilda_check==1) or (number==-1 and tilda_check==0)
+
+def tilda_exception(lst,index,count,tilda_check):
+    if index==len(lst)-1:
+        raise MissingOperandError("~")
+    elif  index!=0 and lst[index-1] in ("<","_") :
+        raise TooMuchOperatorsInARow("~","-")
+    elif tilda_check==1:
+        raise TwoOrMoreTildas()
+    else:
+        return
